31b500137635c0f4d44f3ae938ab0210daf004b2
madhav1602 <madhavaggarwal580@gmail.com>
2025-03-16 10:28:43 +0530

    [Madhav] Add: Restaurant Management System

---
+++ RestaurantManagement.java
@@ -0,0 +1,63 @@
+// Superclass: Person
+class Person {
+    protected String name;
+    protected int id;
+
+    public Person(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public void displayDetails() {
+        System.out.println("Name: " + name + ", ID: " + id);
+    }
+}
+
+// Interface: Worker
+interface Worker {
+    void performDuties();
+}
+
+// Subclass: Chef (inherits Person, implements Worker)
+class Chef extends Person implements Worker {
+    private String specialty;
+
+    public Chef(String name, int id, String specialty) {
+        super(name, id);
+        this.specialty = specialty;
+    }
+
+    @Override
+    public void performDuties() {
+        System.out.println(name + " (Chef) is cooking " + specialty + ".");
+    }
+}
+
+// Subclass: Waiter (inherits Person, implements Worker)
+class Waiter extends Person implements Worker {
+    private int tablesAssigned;
+
+    public Waiter(String name, int id, int tablesAssigned) {
+        super(name, id);
+        this.tablesAssigned = tablesAssigned;
+    }
+
+    @Override
+    public void performDuties() {
+        System.out.println(name + " (Waiter) is serving " + tablesAssigned + " tables.");
+    }
+}
+
+// Main Class
+public class RestaurantManagement {
+    public static void main(String[] args) {
+        Chef chef = new Chef("Gordon Ramsay", 101, "Italian Cuisine");
+        Waiter waiter = new Waiter("John Doe", 202, 5);
+
+        chef.displayDetails();
+        chef.performDuties();
+
+        waiter.displayDetails();
+        waiter.performDuties();
+    }
+}
