55302fc327bbc58125acf953607f978e37e6fa72
madhav1602 <madhavaggarwal580@gmail.com>
2025-03-16 10:10:51 +0530

    [Madhav] Add: Animal hierarchy

---
+++ AnimalHierarchy.java
@@ -0,0 +1,65 @@
+// Superclass: Animal
+class Animal {
+    protected String name;
+    protected int age;
+
+    public Animal(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    // Method to be overridden
+    public void makeSound() {
+        System.out.println(name + " makes a sound.");
+    }
+}
+
+// Subclass: Dog
+class Dog extends Animal {
+    public Dog(String name, int age) {
+        super(name, age);
+    }
+
+    @Override
+    public void makeSound() {
+        System.out.println(name + " barks");
+    }
+}
+
+// Subclass: Cat
+class Cat extends Animal {
+    public Cat(String name, int age) {
+        super(name, age);
+    }
+
+    @Override
+    public void makeSound() {
+        System.out.println(name + " meows");
+    }
+}
+
+// Subclass: Bird
+class Bird extends Animal {
+    public Bird(String name, int age) {
+        super(name, age);
+    }
+
+    @Override
+    public void makeSound() {
+        System.out.println(name + " chirps");
+    }
+}
+
+// Main Class
+public class AnimalHierarchy {
+    public static void main(String[] args) {
+        Animal dog = new Dog("Buddy", 3);
+        Animal cat = new Cat("Whiskers", 2);
+        Animal bird = new Bird("Tweety", 1);
+
+
+        dog.makeSound();
+        cat.makeSound();
+        bird.makeSound();
+    }
+}
